233e07a914e16772a5bff9b99b81c547a0794652
justintham <81354747+justintham@users.noreply.github.com>
2022-04-07 22:11:58 +0800

    Add files via upload

--- JCTY.py
+++ JCTY.py
@@ -60,7 +60,7 @@ def AddEmp():
     phone_number = request.form['phone_number']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
