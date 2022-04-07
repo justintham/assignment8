2311fef04d37f72b7268bd26bbaf8224c3e80df4
justintham <81354747+justintham@users.noreply.github.com>
2022-04-07 21:45:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+customhost = "employee.cjvcbowyawpi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "justinthamjiawei-employee"
+customregion = "us-east-1"
+cloudfront = "d35qmwwf2o1oxt.cloudfront.net"
+
+
